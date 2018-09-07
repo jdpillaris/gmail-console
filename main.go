8930e236915d95682bbdf6c35469ff7f719c5f20
Jaidev <jdpilaris@gmail.com>
2018-09-07 08:36:00 +0800

    Validate start/end dates

--- main.go
+++ main.go
@@ -15,18 +15,32 @@ func init() {
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
+
 	fmt.Print("Start of the retrieval period (DD-MM-YYYY): ")
 	startDateStr, _ := reader.ReadString('\n')
 	startDateStr = strings.TrimSuffix(startDateStr, "\n")
-	startDate, _ := time.Parse("02-01-2006", startDateStr)
-
 	fmt.Print("End of the retrieval period (DD-MM-YYYY): ")
 	endDateStr, _ := reader.ReadString('\n')
 	endDateStr = strings.TrimSuffix(endDateStr, "\n")
+
+	while !isValid(startDateStr, endDateStr) {
+		fmt.Print("Start of the retrieval period (DD-MM-YYYY): ")
+		startDateStr, _ := reader.ReadString('\n')
+		startDateStr = strings.TrimSuffix(startDateStr, "\n")
+		fmt.Print("End of the retrieval period (DD-MM-YYYY): ")
+		endDateStr, _ := reader.ReadString('\n')
+		endDateStr = strings.TrimSuffix(endDateStr, "\n")
+	}
+
+	startDate, _ := time.Parse("02-01-2006", startDateStr)
 	endDate, _ := time.Parse("02-01-2006", endDateStr)
 
 	inbox := new(controllers.Inbox)
 	inbox.Create()
 	inbox.StoreMessages(startDate, endDate)
+}
 
+// isValid returns true. Assume that the correct start/end dates are entered 
+func isValid(begin, end string) {
+	return true
 }
